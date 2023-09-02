ef42b5de34cc28cde7a0687cf03e667c392f8816
nasvank7 <nasvank7@gmail.com>
2023-09-02 16:46:56 +0530

    env

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-    cloud_name: 'dwrbpntpl',
-    api_key: '535994212244639',
-    api_secret: 'dq6KcAhB4IYvYLpjO1Nkf1di3aY',
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
 })
 
 export default cloudinary;
